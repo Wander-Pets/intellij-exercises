dc048c55bf03f9b05f9d2844402453c9b043f74d
Elijah Santos <65294557+jah-peg@users.noreply.github.com>
2021-04-06 17:41:29 +0800

    Update Main.java

--- src/udemyclass/Main.java
+++ src/udemyclass/Main.java
@@ -3,5 +3,9 @@ package udemyclass;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello World!");
+
+        for (int i = 0; i <= 5; i++) {
+        	System.out.println("This is number " + i);
+        }
     }
 }
